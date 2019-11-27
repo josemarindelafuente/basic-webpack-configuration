877b7545767d773d1c8c491b4bfb3e41d5d948f9
jose Marin <jose.marindelafuente@gmai.com>
2019-11-27 11:02:33 -0300

    primer commit / ejemplo realizado siguiendo el video https://www.youtube.com/watch?v=3LZOL65sxhU

---
+++ README.md
@@ -0,0 +1,7 @@
+Ejecutar primero `npm install` 
+
+luego `npm start:dev` 
+
+Basados en este tutorial: https://www.youtube.com/watch?v=3LZOL65sxhU
+Setting up Webpack and Webpack Dev Server (2019)
+
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js', //nombre del archivo de origen,
+
+  mode: 'development', //modo desarrollo 
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  
+  devServer: {
+      open: true
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'), //Dist es la carpeta donde se generara el archivo bundle.js
+    filename: 'bundle.js' //nombre del archivo de salida
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),  
+    new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+    })
+  ]
+};
